e10ce622678e53170ac82caa4b737b384e95b950
lxx14 <lxx1427@gmail.com>
2019-05-16 12:55:41 +0300

    create single page content

--- src/components/pages/SinglePost/index.js
+++ src/components/pages/SinglePost/index.js
@@ -3,13 +3,25 @@ import { NavLink } from 'react-router-dom';
 import { connect } from 'react-redux';
 import './style.scss';
 
-const SinglePost = () => {
+const SinglePost = (props) => {
+  console.log(props.onePost);
+  const {author, body, date, title} = props.onePost;
   return (
     <div className='page single-page'>
-      <h1>SinglePost</h1>
+      <h1>{title}</h1>
+      <p>{body}</p>
+      <p className='author'><span>{author}</span> <span>{date}</span></p>
       <NavLink to="/"><i className="fas fa-angle-left"></i> Return</NavLink>
     </div>
   )
 }
 
-export default SinglePost;
+const mapStateToProps = (state, ownProps) => {
+  const singleId = ownProps.match.params.id;
+  const onePost = state.postsContainer.posts.find( item => parseFloat(singleId) === item.id);
+  return {
+    onePost
+  }
+}
+
+export default connect(mapStateToProps)(SinglePost);
